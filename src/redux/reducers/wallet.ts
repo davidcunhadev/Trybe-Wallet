cbfeb292568eee4c0e8a050c9c9a4d5eeed25c78
¨David <davidl1199@hotmail.com>
2023-07-24 19:25:07 -0300

    fix lint

--- src/redux/reducers/wallet.ts
+++ src/redux/reducers/wallet.ts
@@ -1,11 +1,11 @@
-import { AnyAction } from 'redux';
+// import { AnyAction } from 'redux';
 // import { WalletInfos } from '../../types';
 
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-const WALLET_INITIAL_STATE = {
-  moeda: 'euro',
-};
+// const WALLET_INITIAL_STATE = {
+//   moeda: 'euro',
+// };
 
-const walletReducer = (state = WALLET_INITIAL_STATE, action: AnyAction) => state;
+// const walletReducer = (state = WALLET_INITIAL_STATE, action: AnyAction) => state;
 
-export default walletReducer;
+// export default walletReducer;
