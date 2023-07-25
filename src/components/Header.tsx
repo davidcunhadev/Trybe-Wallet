1e9b47d7cf220e9b45a047de3ab7bcec484e9278
¨David <davidl1199@hotmail.com>
2023-07-25 15:05:00 -0300

    Task 2 done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,25 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '../types';
+
 function Header() {
-  return <div>Header</div>;
+  const userEmail = useSelector((state: RootState) => state.user.email);
+  const expenseValue = 0;
+
+  return (
+    <section>
+      <p data-testid="email-field">
+        Email:
+        {' '}
+        {userEmail}
+      </p>
+      <p data-testid="total-field">
+        Despesa Total: R$
+        {' '}
+        {expenseValue}
+      </p>
+      <p data-testid="header-currency-field">BRL</p>
+    </section>
+  );
 }
 
 export default Header;
